82c7fef544381b3861bd90648f20f52c2e735221
GONRONDEROS <gonzalorond@gmail.com>
2025-04-13 19:49:32 -0300

    segundo ejercicio

--- 05 Funciones/TPN5-Programacion-Ronderos.py
+++ 05 Funciones/TPN5-Programacion-Ronderos.py
@@ -10,3 +10,17 @@ def imprimir_hola_mundo():
 
 #Programa Principal
 imprimir_hola_mundo()
+
+# 2. Crear una función llamada saludar_usuario(nombre) que reciba
+# como parámetro un nombre y devuelva un saludo personalizado.
+# Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
+# principal solicitando el nombre al usuario.
+
+#Definicion de Funciones
+def saludar_usuario(nombre):
+    print(f'Hola {nombre}!')
+
+#Programa Principal
+nombre = input('Por favor ingrese su nombre: ')
+saludar_usuario(nombre)
+
