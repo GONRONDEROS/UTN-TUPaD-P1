d9bb62e1ab090e0b2e3ec150c4af0f943abd21f5
GONRONDEROS <gonzalorond@gmail.com>
2025-04-13 20:43:04 -0300

    octavo ejercicio

--- 05 Funciones/TPN5-Programacion-Ronderos.py
+++ 05 Funciones/TPN5-Programacion-Ronderos.py
@@ -115,7 +115,17 @@ a = float(input('Por favor ingrese el primer numero: '))
 b = float(input('Por favor ingrese el segundo numero: '))
 operaciones_basicas(a,b)
 
+# 8. Crear una función llamada calcular_imc(peso, altura) que reciba el
+# peso en kilogramos y la altura en metros, y devuelva el índice de
+# masa corporal (IMC). Solicitar al usuario los datos y llamar a la función para mostrar el resultado con dos decimales.
 
+#Definicion de Funciones
+def calcular_imc(peso, altura):
+    imc = (peso / (altura**2))
+    print(f'Su IMC es de {imc}')
 
-
+#Programa Principal
+peso = float(input('Por favor ingrese el peso en kg: '))
+altura = float(input('Por favor ingrese altura en m: '))
+calcular_imc(peso, altura)
 
