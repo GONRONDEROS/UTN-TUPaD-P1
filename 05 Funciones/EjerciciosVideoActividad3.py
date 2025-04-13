440976760d9dd44faa0074ffc431e6d6fe043e22
GONRONDEROS <gonzalorond@gmail.com>
2025-04-13 19:17:24 -0300

    cambios luego del video

--- 05 Funciones/EjerciciosVideoActividad3.py
+++ 05 Funciones/EjerciciosVideoActividad3.py
@@ -9,21 +9,26 @@ def informar_si_numero_es_perfecto(numero):
     if es_perfecto(numero):
         print(f'El numero {numero} es perfecto')
     else:
-        print(f'El nmero {numero} MO es perfecto')
-        
-def es_perfecto(numero):
-    aSumar = 0
-    while numero > 1:
-        for i in range(1, numero):
-            if numero % i == 0:
-                aSumar = aSumar + i
-            return aSumar
-    if aSumar == numero:
-        return True
-    else: 
-        return False
+        print(f'El nmero {numero} NO es perfecto')
 
+def obtener_resto(num1, num2):
+    return num1 - num2 * (num1 // num2)
+
+def es_multiplo(x, y):
+    return obtener_resto(x, y) == 0
+
+def sumatoria_divisores_propios(numero):    
+    aSumar = 0
+    for i in range(1, (numero // 2) + 1):
+        if es_multiplo(numero, i):
+            aSumar += i
+    return aSumar
+
+
+def es_perfecto(numero):
+    return sumatoria_divisores_propios(numero) == numero
+    
 
 # Programa Principal
-num = leer_entero_valido('Ingrese un numero natural', 1)
+num = leer_entero_valido('Ingrese un numero natural: ', 1)
 informar_si_numero_es_perfecto(num)
