5e79e26861dd7c13d9f211787040948e20e93acd
GONRONDEROS <gonzalorond@gmail.com>
2025-04-13 19:46:19 -0300

    primer ejercicio

---
+++ 05 Funciones/TPN5-Programacion-Ronderos.py
@@ -0,0 +1,12 @@
+# ACTIVIDADES 
+
+# 1. Crear una función llamada imprimir_hola_mundo que imprima por
+# pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+# programa principal.
+
+#Definicion de Funciones
+def imprimir_hola_mundo():
+    print('Hola Mundo!')
+
+#Programa Principal
+imprimir_hola_mundo()
