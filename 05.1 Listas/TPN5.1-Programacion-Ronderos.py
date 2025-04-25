b7e4e18f34f2a54c58f970a85d1ed670c613ee9e
GONRONDEROS <gonzalorond@gmail.com>
2025-04-25 19:59:59 -0300

    Ejercicios 1 & 2 hechos

---
+++ 05.1 Listas/TPN5.1-Programacion-Ronderos.py
@@ -0,0 +1,21 @@
+#ACTIVIDADES
+#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+
+numeros_multiplos_de_4 = list(range(4,101,4))
+print(numeros_multiplos_de_4)
+
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el 
+# penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+lista_de_5_elementos = [['Estudiantes de La Plata', 'Campeon del Mundo'], 'Veron', 11, 'Bilardo', 'Leon']
+penultimo_elemento = lista_de_5_elementos[-2]
+print(penultimo_elemento)
+
+# 3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
+# pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por
+# ejemplo:
+# lista_vacia = []
+# 4) Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
+# respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
+# en los videos o bien investigar cómo funciona el indexing con números negativos!
+# animales = ["perro", "gato", "conejo", "pez"]
