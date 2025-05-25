0f1d19aa6507bacbda3edfe496ce11167c6aa351
GONRONDEROS <gonzalorond@gmail.com>
2025-05-25 16:55:45 -0300

    Punto uno TP Recursividad

---
+++ 11 Recursividad/TPN6-Programacion-Ronderos.py
@@ -0,0 +1,26 @@
+## ACTIVIDADES ##
+
+#1) Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa
+#función para calcular y mostrar en pantalla el factorial de todos los números enteros
+#entre 1 y el número que indique el usuario
+
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    return num * factorial(num -1)
+
+#2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
+#indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario
+#especifique.
+
+
+
+
+#3) Crea una función recursiva que calcule la potencia de un número base elevado a un
+#exponente, utilizando la fórmula 𝑛**𝑚 = (𝑛) ∗ (𝑛)**(m-1). Prueba esta función en un
+#algoritmo general.
+
+
+
+#4) Crear una función recursiva en Python que reciba un número entero positivo en base
+#decimal y devuelva su representación en binario como una cadena de texto.
