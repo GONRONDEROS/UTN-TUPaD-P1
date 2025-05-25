92126c40a58e0c1225a83b6607fa2b4d2bca3e98
GONRONDEROS <gonzalorond@gmail.com>
2025-05-25 18:38:16 -0300

    se sumaron los problemas del 5 al 8

--- 11 Recursividad/TPN6-Programacion-Ronderos.py
+++ 11 Recursividad/TPN6-Programacion-Ronderos.py
@@ -48,4 +48,44 @@ def decimal_a_binario(base_decimal):
     
 print(f'Resultado de Decimal A Binario con decimal = 10: {decimal_a_binario(10)}')
 
+#5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
+#cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no
+#lo es.
+#Requisitos:
+#La solución debe ser recursiva.
+#No se debe usar [::-1] ni la función reversed().
 
+
+
+
+#6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
+#número entero positivo y devuelva la suma de todos sus dígitos.
+#Restricciones:
+#No se puede convertir el número a string.
+#Usá operaciones matemáticas (%, //) y recursión.
+#Ejemplos:
+#suma_digitos(1234) → 10 (1 + 2 + 3 + 4)
+#suma_digitos(9) → 9
+#suma_digitos(305) → 8 (3 + 0 + 5)
+
+
+
+#7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
+#bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
+#último nivel con un solo bloque.
+#Escribí una función recursiva contar_bloques(n) que reciba el número de bloques en el
+#nivel más bajo y devuelva el total de bloques que necesita para construir toda la
+#pirámide.
+#Ejemplos:
+#contar_bloques(1) → 1 (1)
+#contar_bloques(2) → 3 (2 + 1)
+#contar_bloques(4) → 10 (4 + 3 + 2 + 1)
+
+
+#8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
+#número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
+#aparece ese dígito dentro del número.
+#Ejemplos:
+#contar_digito(12233421, 2) → 3
+#contar_digito(5555, 5) → 4 
+#contar_digito(123456, 7) → 0 
