4f661b60ff3d4e53bbeda40c9a8728be16564f29
GONRONDEROS <gonzalorond@gmail.com>
2025-06-11 20:57:06 -0300

    ejercicio 1 tp6

---
+++ 06 Datos complejos/TPN6-Programacion-Ronderos.py
@@ -0,0 +1,9 @@
+# Actividades
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+1450}
+print(precios_frutas)
+
+precios_frutas.update({'Naranja': 1200, 'Manzana': 1500, 'Pera': 2300})
+print(precios_frutas)
+
