3efe30996f4c3cf23627b5cc1a64aae6c13b5114
GONRONDEROS <gonzalorond@gmail.com>
2025-06-13 19:04:50 -0300

    ejercicio 5

--- 06 Datos complejos/TPN6-Programacion-Ronderos.py
+++ 06 Datos complejos/TPN6-Programacion-Ronderos.py
@@ -48,3 +48,20 @@ def consulta(agenda):
 
 agenda()
 
+#5
+
+frase = input("Ingresá una frase: ")
+palabras = frase.split()
+
+unicas = set(palabras)
+print("Palabras únicas:", unicas)
+
+recuento = {}
+for palabra in palabras:
+    if palabra in recuento:
+        recuento[palabra] += 1
+    else:
+        recuento[palabra] = 1
+
+print("Recuento:", recuento)
+
